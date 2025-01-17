db35a0b820a2222c98dc66c5e9f241ba44cc64b8
Febryan Riyadi <115878138+Brazer27@users.noreply.github.com>
2025-01-17 18:27:37 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="max-w-7xl mx-auto flex flex-col relative">
+    
+    <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
+      <div class="container mx-auto flex flex-wrap items-center justify-between">
+        <button @click="redirectToHome" class="flex">
+          <span class="self-center text-lg text-[#70ffc8] font-semibold whitespace-nowrap fadein-bot hover:text-teal-100">febryanriyadi;</span>
+        </button>
+        <div class="flex md:order-2 fadein-bot">
+  
+  <router-link to="/login" class="text-[#70ffc8] hover:text-teal-100 mr-4">
+    <button class="bg-[#70ffc8] text-gray-700 py-1 px-3 rounded-full hover:bg-teal-100 transition">
+      Login/Register
+    </button>
+  </router-link>
+
+  <a href="https://github.com/Brazer27">
+    <img class="w-9 rounded-full" src="https://www.freeiconspng.com/img/910" alt="profile">
+  </a>
+  </div>
+        <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
+          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <router-link to="/"
+                class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                aria-current="page">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about"
+                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                >About</router-link>
+            </li>
+            <li>
+              <router-link to="/portfolio"
+                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                >Portfolio</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="md:mt-[100px]">
+      <router-view />
+    </div>
+  </div>
+  <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
+  <nav class="flex justify-around py-4 text-xs">
+    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
+    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
+    <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
+  </nav>
+</footer>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToHome() {
+      this.$router.push('/')
+    }
+  },
+}
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+  width: 5px; /* for vertical scrollbar */
+  height: 5px; /* for horizontal scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: hsl(240, 1%, 17%);
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #70ffc8;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-button { width: 20px; }
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background: hsl(0, 0%, 7%);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active {
+  display: inline-flex;
+  flex-direction: column;
+  color: white;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active::after {
+  display: inline-block;
+  content: "";
+  margin-top: 0.08em;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: #70ffc8;
+}
+
+nav a.router-link-exact-active:hover {
+  color: white;
+}
+
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,112 @@
+<script>
+export default {
+  data() {
+    return {
+      tech: [
+        {
+          id: 1,
+          name: 'LinkedIN',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/512/174/174857.png',
+          status: 'work',
+          url: 'https://www.linkedin.com/in/febryan-riyadi-2705n412'
+        },
+        {
+          id: 2,
+          name: 'GitHub',
+          imageUrl: 'https://img.icons8.com/m_sharp/200/FFFFFF/github.png',
+          status: 'project list',
+          url: 'https://github.com/Brazer27'
+        },
+        {
+          id: 3,
+          name: 'E-mail',
+          imageUrl: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png',
+          status: 'contact me',
+          url: 'mailto:febryanriyadi03@gmail.com'
+        },
+        {
+          id: 4,
+          name: 'Instagram',
+          imageUrl: 'https://cdn-icons-png.freepik.com/256/15789/15789364.png?semt=ais_hybrid',
+          status: 'social media',
+          url: 'https://www.instagram.com/febryan_4123?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
+        }
+      ],
+    };
+  },
+  methods: {
+    openLink(url) {
+      window.open(url, '_blank');
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-teal-50 mx-3">
+    <article data-page="about">
+      <header>
+        <div class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
+          <h4>My Contacts</h4>
+          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-teal-300">
+            Feel free to contact me using the information below.
+          </h4>
+        </div>
+      </header>
+      <section>
+        <div class="grid grid-cols-2 gap-3 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12 fade-zoom-in">
+          <!-- Loop through the tech array -->
+          <div v-for="item in tech" :key="item.id">
+            <!-- Added @click event to handle link opening -->
+            <div @click="openLink(item.url)" class="item-tech flex cursor-pointer items-center gap-2 rounded border border-teal-200 px-2 py-2 hover:bg-teal-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+              <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                <img :src="item.imageUrl" alt="Contact Logo" loading="lazy" class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]" />
+              </div>
+              <div class="flex items-center text-sm md:text-base lg:text-lg">
+                <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0">{{ item.name }}</div>
+                <div class="status-tech opacity-0 absolute mt-5 text-[10px] text-teal-200 transition-all duration-300 md:text-xs lg:text-sm">
+                  {{ item.status }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="text-2xl text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
+          <h6>My Phone Number</h6>
+          <h7 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-teal-300">
+            +62 822 1543 6168
+          </h7>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+
+<style scoped>
+/* Add custom styles */
+.item-card:hover {
+  transition: transform 0.3s ease;
+  transform: translateY(-8px);
+}
+
+svg:hover {
+  stroke: #70ffc8;
+}
+
+@keyframes fadeZoomIn {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+.fade-zoom-in {
+  animation: fadeZoomIn 2s ease-in-out;
+}
+</style>
